2173bc535f0cb5e36cf1a999704f22629358c51c
talfumier <henri.talfumier@gmail.com>
2024-01-20 20:06:31 +0100

    Added focus handler + static property update on component destroy in counter.component.ts

--- src/app/cart/counter/counter.component.ts
+++ src/app/cart/counter/counter.component.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Component, ElementRef, Input,OnInit,ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input,OnDestroy,OnInit,ViewChild } from '@angular/core';
+import _ from "lodash";
 import { CartService } from '../../services/cart.service';
 
 @Component({
@@ -6,7 +7,7 @@ import { CartService } from '../../services/cart.service';
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css', 
 })
-export class CounterComponent implements OnInit, AfterViewInit {
+export class CounterComponent implements OnInit, AfterViewInit, OnDestroy{
   @Input() id:string=""; 
   @Input() type:string[]=[]; 
   @ViewChild('counterInput') input: ElementRef={} as ElementRef;
@@ -23,6 +24,9 @@ export class CounterComponent implements OnInit, AfterViewInit {
    ngAfterViewInit(): void {
      CounterComponent.elts.push(this.input);
    }
+   ngOnDestroy(): void {
+     CounterComponent.elts.splice(CounterComponent.elts.indexOf(this.input),1);
+   }
   
   get count():number{
     return this._count;
@@ -30,15 +34,17 @@ export class CounterComponent implements OnInit, AfterViewInit {
   set count(value:number) {
     this._count=value;
   }
-  handleCartIncrement(cs:number){
-    if(cs<0 && this._count===0) return;
+  handleFocus() {
     const cartItem=this.service.cart[this.service.getCartIndex(this.id)];
     this._count=cartItem?cartItem.qty:0;
+  }
+  handleCartIncrement(cs:number){
+    if(cs<0 && this._count===0) return;
     this._count+=cs;
     if(this.type[0]==="remove"){ 
       this.service.cart[this.service.getCartIndex(this.id)].qty=this._count;
       CounterComponent.elts.map((elt) => {
-        if(elt.nativeElement.id==="input-counter-add") elt.nativeElement.value=this._count;
+        if((elt.nativeElement.id as string).includes("input-counter-add")) elt.nativeElement.value=this._count;
       })
     }
   }  
