dc3ae1f30b2441fe9e935767fc8afa1021572fec
talfumier <henri.talfumier@gmail.com>
2024-01-21 20:12:46 +0100

    Minor change in app.component.ts

--- src/environments/set-env.ts
+++ src/environments/set-env.ts
@@ -4,7 +4,6 @@ const setEnv = () => {
 // Configure Angular `environment.ts` file path
   const targetPath = './environment.ts'//'./src/environments/environment.ts';
 // Load node modules
-  // const colors = require('colors');
   const appVersion = require('../../package.json').version;
   require('dotenv').config({
     path: 'src/environments/.env'
