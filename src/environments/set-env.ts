1584d31cf639fbee08116023adcf30e987dc4786
talfumier <henri.talfumier@gmail.com>
2024-01-22 15:20:24 +0100

    Debug env variable setting

--- src/environments/set-env.ts
+++ src/environments/set-env.ts
@@ -4,16 +4,15 @@ const setEnv = () => {
 // Configure Angular `environment.ts` file path
   const targetPath = './environment.ts'//'./src/environments/environment.ts';
 // Load node modules
-  const appVersion = require('../../package.json').version;
-  require('dotenv').config({
-    path: 'src/environments/.env'
-  });
+  // const appVersion = require('../../package.json').version;
+  // require('dotenv').config({
+  //   path: 'src/environments/.env'
+  // });
 // `environment.ts` file structure, FB_API_KEY must be defined as an environment variable in the hosting server
   const envConfigFile = `export const environment = {
-  firebaseApiKey: '${process.env["FB_API_KEY"]}',
-  production: true,
-};
-`;
+    firebaseApiKey: '${process.env["FB_API_KEY"]}',
+    production: true,
+  };`;
   console.log('The file `environment.ts` will be written with the following content: \n');
   writeFile(targetPath, envConfigFile, (err:Error) => {
     if (err) {
